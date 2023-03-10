b6a2d194c7c07fb284e4ad5ab9d1ec515ec9042d
commandus <andrei.i.ivanov@commandus.com>
2023-03-10 11:58:11 +0900

    Add tests

--- MeasureUnit.cpp
+++ MeasureUnit.cpp
@@ -3,3 +3,103 @@
 //
 
 #include "MeasureUnit.h"
+
+#define MAX_POW10 11
+
+static const std::string prefixes[MAX_POW10] {
+    "",
+    "k",
+    "M",
+    "G",
+    "Т",
+    "P",
+    "E",
+    "Z",
+    "Y",
+    "R",
+    "Q"
+};
+
+static const std::string prefixesPart[MAX_POW10] {
+    "",
+    "m",
+    "µ",
+    "n",
+    "p",
+    "f",
+    "a",
+    "z",
+    "y",
+    "r",
+    "q"
+};
+
+static int measurePow10[] {
+    0,
+    -9,
+    -9
+};
+
+static const std::string symNamesRU[] {
+    "R",
+    "C",
+    "L"
+};
+
+static const std::string unitNamesRU[] {
+    "Ом",
+    "Ф",
+    "Гн"
+};
+
+MeasureUnit::MeasureUnit()
+{
+
+}
+
+MeasureUnit::~MeasureUnit()
+{
+
+}
+
+std::string MeasureUnit::sym(MEASURE measure) {
+    return symNamesRU[measure];
+}
+
+std::string MeasureUnit::unit(MEASURE measure)
+{
+    return unitNamesRU[measure];
+}
+
+int pow10(MEASURE measure)
+{
+    return measurePow10[measure];
+}
+
+std::string val1000(uint64_t value, int initialPow10) {
+    uint64_t v = value;
+    int initialPowIdx = initialPow10 / 3;
+    for (auto i = 1; i < MAX_POW10; i++) {
+        v /= 1000;
+        if (v < 1000) {
+            int idx = i - initialPowIdx;
+            if (idx >= 0)
+                return std::to_string(v) + prefixes[idx];
+            else
+                return std::to_string(v) + prefixesPart[-idx];
+        }
+    }
+}
+
+std::string MeasureUnit::value(MEASURE measure, uint64_t value)
+{
+    int p = measurePow10[measure];
+    if (p == 0)
+        return val1000(value);
+    return "";
+}
+
+double MeasureUnit::val(MEASURE measure, uint64_t value)
+{
+    return 0;
+}
--- MeasureUnit.h
+++ MeasureUnit.h
@@ -5,9 +5,23 @@
 #ifndef RCR_MEASUREUNIT_H
 #define RCR_MEASUREUNIT_H
 
+#include <string>
+
+typedef enum {
+    M_R = 1,
+    M_C = 2,
+    M_L = 3
+} MEASURE;
 
 class MeasureUnit {
-
+public:
+    MeasureUnit();
+    virtual ~MeasureUnit();
+    std::string sym(MEASURE measure);
+    std::string unit(MEASURE measure);
+    int pow10(MEASURE measure);
+    std::string value(MEASURE measure, uint64_t value);
+    double val(MEASURE measure, uint64_t value);
 };
 
 
