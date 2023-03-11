8ff0a0adf79a3cbe686493e2c7b0c4c22f2db6f2
commandus <andrei.i.ivanov@commandus.com>
2023-03-11 17:54:19 +0900

    2

---
+++ RcrCredentials.cpp
@@ -0,0 +1,132 @@
+/*
+ * TicketCredentials.cpp
+ */
+
+#include <iostream>
+#include <sstream>
+#include <glog/logging.h>
+
+#include "utilstring.h"
+#include "TicketCredentials.h"
+
+using grpc::SslServerCredentialsOptions;
+
+const char *META_AUTH_TICKET = "x-auth-ticket";
+const std::string &META_AUTH_TICKET_STR(META_AUTH_TICKET);
+const grpc::string_ref META_AUTH_TICKET_STR_REF(META_AUTH_TICKET_STR);
+const char *META_AUTH_USER = "x-auth-user";
+const char DELIM_TICKET = ' ';
+const char *METADATA_PATH = ":path";
+// this is special case when no SSL certificate is not required
+const char *NOSSL_GETCERTIFICATE_METADATA_PATH = "/stickychat.StickyChat/addUser";
+
+const char *RcrMetadataCredentialsPlugin::getMetaTicketName()
+{
+	return META_AUTH_TICKET;
+}
+
+const std::string& RcrMetadataCredentialsPlugin::getMetaTicketNameStr()
+{
+	return META_AUTH_TICKET_STR;
+}
+
+const grpc::string_ref RcrMetadataCredentialsPlugin::getMetaTicketNameRef()
+{
+	return META_AUTH_TICKET_STR_REF;
+}
+
+const char *RcrMetadataCredentialsPlugin::getMetaUserName()
+{
+	return META_AUTH_USER;
+}
+
+const grpc::string ERR_INV_TICKET("Invalid ticket");
+const grpc::string ERR_INV_CERTIFICATE("Invalid certificate");
+
+// Client creates token
+RcrMetadataCredentialsPlugin::RcrMetadataCredentialsPlugin(const std::string& username, const std::string &password)
+{
+	// ticket is combination of user name and password
+	std::stringstream ssticket;
+	ssticket << username << "|" << password;
+	mClientTicket = ssticket.str();
+}
+
+// Client insert credentials ticket here
+grpc::Status RcrMetadataCredentialsPlugin::GetMetadata(grpc::string_ref service_url, grpc::string_ref method_name,
+		const grpc::AuthContext& channel_auth_context,
+		std::multimap<grpc::string, grpc::string>* metadata)
+{
+	metadata->insert(std::make_pair(META_AUTH_TICKET, mClientTicket));
+	return Status::OK;
+}
+
+void RcrAuthMetadataProcessor::debugPrintAuthContext(std::ostream &strm, AuthContext* context)
+{
+	for (grpc::AuthPropertyIterator it(context->begin()); it != context->end(); ++it)
+	{
+		strm << (*it).first << ":" << (*it).second;
+	}
+}
+
+/**
+  * server side
+  * Check user by SSL certificate, or token.
+  * x509_common_name
+  * There are no password check.
+  */
+Status RcrAuthMetadataProcessor::Process(
+	const InputMetadata& auth_metadata, AuthContext* context,
+	OutputMetadata* consumed_auth_metadata, OutputMetadata* response_metadata
+)
+{
+	if (!mRcrValidator)
+		return Status::OK;
+	int f;
+	int64_t uid, oid;
+	if (mIsRequestUserCertificate)
+	{
+		// check certificate CN if server required
+		std::vector<grpc::string_ref> id = context->GetPeerIdentity();
+		// client may provide 0, 1 or more identities.
+		bool ok = false;
+		for (std::vector<grpc::string_ref>::iterator it = id.begin(); it != id.end(); it++)
+		{
+			if (mRcrValidator->onCheckCertificateCN(*it, &f, &oid, &uid))
+			{
+				ok = true;
+				break;
+			}
+		}
+		if (!ok)
+		{
+			// This is special case	:path: /nfcreceipt.NFCReceipt/getNice
+			auto metapath = auth_metadata.find(METADATA_PATH);
+			if (metapath != auth_metadata.end())
+			{
+				std::string p(metapath->second.data());
+				if (p == NOSSL_GETCERTIFICATE_METADATA_PATH)
+					return Status::OK;
+			}
+			return Status(grpc::StatusCode::UNAUTHENTICATED, ERR_INV_CERTIFICATE);
+		}
+	}
+	else
+	{
+		// try get from metadata
+		auto metaAuth = auth_metadata.find(RcrMetadataCredentialsPlugin::getMetaTicketName());
+		if (metaAuth == auth_metadata.end())
+			return Status(grpc::StatusCode::UNAUTHENTICATED, ERR_INV_TICKET);
+
+		if (!mRcrValidator->onCheckTicket(metaAuth->second, &f, &uid, &oid))
+			return Status(grpc::StatusCode::UNAUTHENTICATED, ERR_INV_TICKET);
+	}
+
+	std::stringstream ss;
+	ss << oid << DELIM_TICKET << uid << DELIM_TICKET << f;
+	std::string s(ss.str());
+	context->AddProperty(RcrMetadataCredentialsPlugin::getMetaUserName(), s);
+	// LOG(INFO) << "Process: " << s;
+	return Status::OK;
+}
+
---
+++ RcrCredentials.h
@@ -0,0 +1,123 @@
+/*
+ * RcrCredentials.h
+ *
+ *  Created on: 18.02.2016
+ *      Author: andrei
+ */
+
+#ifndef OTPCREDENTIALSAUTHENTIFICATOR_H_
+#define OTPCREDENTIALSAUTHENTIFICATOR_H_
+
+#define GRPC_OVERRIDE
+
+#include <string>
+#include <memory>
+
+#include <grpc++/security/auth_metadata_processor.h>
+#include <grpc++/security/credentials.h>
+#include <grpc++/security/server_credentials.h>
+
+using grpc::ServerCredentials;
+using grpc::CallCredentials;
+using grpc::Status;
+using grpc::AuthContext;
+using grpc::AuthMetadataProcessor;
+using grpc::MetadataCredentialsPlugin;
+
+#define ORG_ROLE_MASK_SVC_ADMIN		1
+#define ORG_ROLE_MASK_ORG_ADMIN		2
+#define ORG_ROLE_MASK_ORG_USER		4
+#define ORG_ROLE_MASK_USER		8
+
+/**
+ * Interface
+ */
+class RcrValidator
+{
+public:
+	// typedef bool (RcrValidator:: *CheckCertificateCNCallback)(const grpc::string_ref &value);
+	// typedef bool (RcrValidator:: *CheckTicketCallback)(const grpc::string_ref &ticket);
+
+	/// abstract method checks certificate
+	virtual bool onCheckCertificateCN(
+		const grpc::string_ref &value,
+		int *retflags,					///< 1- identified user, 0- unidentified.
+		int64_t *retorgid, 				///< return group id. Reserved. Must be 0.
+		int64_t *retuserid				///< return user id. 0- User is not valid.
+	) = 0;
+	/// abstract method checks security ticket set by client (not used now)
+	virtual bool onCheckTicket(
+		const grpc::string_ref &ticket,
+		int *retflags, 					///< 1- identified user, 0- unidentified.
+		int64_t *retorgid, 				///< return group id. Reserved. Must be 0.
+		int64_t *retuserid				///< return user id. 0- User is not valid.
+	) = 0;
+	/// abstract method checks user/password pair set by client. Not used now.
+	virtual bool onCheckPassword(
+		const grpc::string_ref &user,
+		const grpc::string_ref &password,
+		int *retflags, 					///< 1- identified user, 0- unidentified.
+		int64_t *retorgid, 				///< return group id. Reserved. Must be 0.
+		int64_t *retuserid				///< return user id. 0- User is not valid.
+	) = 0;
+};
+
+/**
+  *	server side ticket authenticator
+  */
+class RcrAuthMetadataProcessor : public AuthMetadataProcessor
+{
+private:
+	bool mIsRequestUserCertificate;
+	RcrValidator *mRcrValidator;
+	void debugPrintAuthContext(std::ostream &strm, AuthContext* context);
+public:
+	RcrAuthMetadataProcessor(
+			bool isblocking,
+			bool isRequestUserCertificate,
+			RcrValidator *RcrValidator)
+	: mIsRequestUserCertificate(isRequestUserCertificate),
+	  mRcrValidator(RcrValidator)
+	{
+
+	};
+
+	// Interface implementation
+	bool IsBlocking() const GRPC_OVERRIDE { return true; }
+	bool isRequestUserCertificate() const { return mIsRequestUserCertificate; }
+	Status Process(const InputMetadata& auth_metadata, AuthContext* context,
+			OutputMetadata* consumed_auth_metadata,
+			OutputMetadata* response_metadata) GRPC_OVERRIDE;
+};
+
+/*
+ *	client side plugin provides metadata
+ */
+class RcrMetadataCredentialsPlugin : public MetadataCredentialsPlugin
+{
+private:
+	grpc::string mClientTicket;
+public:
+	/// Return meta name used in authentication as char*
+	static const char* getMetaTicketName();
+	/// Return meta name used in authentication as std::string
+	static const std::string& getMetaTicketNameStr();
+	/// Return meta name used in authentication as string_ref
+	static const grpc::string_ref getMetaTicketNameRef();
+	/// Return meta user name used in authentication as char*
+	static const char* getMetaUserName();
+	/// client authentication. Client creates token
+	RcrMetadataCredentialsPlugin(
+		const std::string& username,
+		const std::string &password
+	);
+	/// Client insert credentials ticket here
+	grpc::Status GetMetadata(
+		grpc::string_ref service_url,
+		grpc::string_ref method_name,
+		const grpc::AuthContext& channel_auth_context,
+		std::multimap<grpc::string, grpc::string>* metadata		///< Insert ticket here
+	) override;
+};
+
+#endif
