e19d4e43f86d4501244ea44950e029ebe9bc7267
commandus <andrei.i.ivanov@commandus.com>
2023-03-13 16:36:33 +0900

    1

---
+++ RCQuery.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andrei on 13.03.23.
+//
+
+#include "RCQuery.h"
---
+++ RCQuery.h
@@ -0,0 +1,14 @@
+//
+// Created by andrei on 13.03.23.
+//
+
+#ifndef RCR_RCQUERY_H
+#define RCR_RCQUERY_H
+
+
+class RCQuery {
+
+};
+
+
+#endif //RCR_RCQUERY_H
