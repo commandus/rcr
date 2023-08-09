09390dc0a6318533b0e56ca6f33a39c30b566d09
Andrei <andrei.i.ivanov@gmail.com>
2023-08-09 16:00:10 +0900

    1

--- odb/odb-views.h
+++ odb/odb-views.h
@@ -10,9 +10,9 @@ namespace rcr
 {
 	// --------------------- Native views ---------------------
 #pragma db view query("SELECT count(id) FROM \"Card\" WHERE (?)")
-    struct CardCount {
-        std::size_t count;
-    };
+struct CardCount {
+    std::size_t count;
+};
 
 // WHERE (?) LIMIT(?) OFFSET(?)
 #pragma db view query("SELECT id, name, uname, symbol_id, nominal FROM \"Card\" WHERE (?) ORDER BY uname, nominal")
@@ -23,5 +23,20 @@ struct CardQuery {
     uint64_t symbol_id;
     uint64_t nominal;
 };
+
+#pragma db view query("SELECT id, dt, user_id, package_id, operation_symbol, value FROM \"Journal\" WHERE (?) order by id desc")
+struct JournalQuery {
+    uint64_t id;
+    uint64_t dt;
+    uint64_t user_id;
+    uint64_t package_id;
+    std::string operation_symbol;
+    int64_t value = 6;                            ///< amount
+};
+
+#pragma db view query("SELECT count(id) FROM \"Journal\" WHERE (?)")
+    struct JournalCount {
+        std::size_t count;
+    };
 
 }
